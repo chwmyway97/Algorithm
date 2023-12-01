25565f60105adaee0be3aaafa497c4e63a324475
chocho <chwmyway@gmail.com>
2023-12-01 15:33:25 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,23 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        bw.write(String.valueOf(a - 543));
+
+        // BufferedWriter의 버퍼를 비워서 출력
+        bw.flush();
+
+        // BufferedWriter를 닫아서 자원을 해제
+        bw.close();
+    }
+
+}
