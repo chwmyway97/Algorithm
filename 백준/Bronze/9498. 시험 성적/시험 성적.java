4db27d0ab00ac99ffc2c5a76ea7b210079226682
chocho <chwmyway@gmail.com>
2023-12-04 23:14:02 +0900

    [Bronze V] Title: 시험 성적, Time: 204 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -14,20 +14,11 @@ public class Main {
         a = sc.nextInt();
 //        b = sc.nextInt();
 
-        if(a >= 90) {
-
-            bw.write("A");
-
-        }else if (80<= a ){
-            bw.write("B");
-        }else if (70<= a ){
-            bw.write("C");
-        }else if (60<= a ){
-            bw.write("D");
-        }else{
-            bw.write("F");
-        }
-
+        if(a >= 90) bw.write("A");
+        else if (80<= a ) bw.write("B");
+        else if (70<= a ) bw.write("C");
+        else if (60<= a ) bw.write("D");
+        else bw.write("F");
 
         // BufferedWriter의 버퍼를 비워서 출력
         bw.flush();
@@ -35,5 +26,10 @@ public class Main {
         // BufferedWriter를 닫아서 자원을 해제
         bw.close();
     }
+    /* 이 방법도 있고 다른방법
+    if(90 <= a && a <= 100) 으로 하는 방법이 있다
+    코틀린 에서는 if(90<= a <=100) 되던걸로 기억하는데 음... 나중에 확인해봐야겠다.
+     */
+
 
 }
